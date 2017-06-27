b22a6b3c6f85b60ac28aa8cdb986d8b4398ad4c4
Frank Greguska <francis.greguska@jpl.nasa.gov>
2017-06-27 13:45:44 -0700

    using new settings variable to get the schema name and table name of the user table

--- esgf_auth/openid.py
+++ esgf_auth/openid.py
@@ -4,6 +4,8 @@ This module contains utility functions for OpenID handling.
 """
 
 from django.db import connections
+from django.conf import settings
+from psycopg2 import sql
 
 
 def django_user_to_openid(user):
@@ -11,5 +13,8 @@ def django_user_to_openid(user):
     Converts a Django user to an OpenID using information in the ESGF user database.
     """
     with connections['userdb'].cursor() as cursor:
-        cursor.execute('SELECT openid FROM esgf_security.user WHERE username = %s', [user.username])
+        cursor.execute(sql.SQL("SELECT openid FROM {}.{} WHERE username = %s")
+                       .format(sql.Identifier(settings.ESGF_USERDB_USER_SCHEMA),
+                               sql.Identifier(settings.ESGF_USERDB_USER_TABLE)),
+                       [user.username])
         return cursor.fetchone()[0]
